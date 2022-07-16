cf92d9c9df4d1a8b4736c785576a6900395f1f45
ykttdn <38461803+ykttdn@users.noreply.github.com>
2022-07-16 17:50:47 +0900

    Clone codes from "L5-start"

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const app = Vue.createApp({
   data() {
-      return {
-          product: 'Socks',
-          image: './assets/images/socks_blue.jpg',
-          inventory: 100,
-          onSale: true
-      }
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester']
+    }
   }
 })
