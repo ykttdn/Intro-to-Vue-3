6ddb6f28a1ccbb8dfe2af247dfc60825dc302e91
ykttdn <38461803+ykttdn@users.noreply.github.com>
2022-07-16 16:52:43 +0900

    Clone codes from "L4-start"

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const app = Vue.createApp({
   data() {
-    return {
-      product: 'Socks',
-      image: './assets/images/socks_green.jpg',
-      url: 'https://www.vuemastery.com/'
-    }
+      return {
+          product: 'Socks',
+          image: './assets/images/socks_blue.jpg'
+      }
   }
 })
