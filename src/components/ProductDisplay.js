6754ed6ca61088ef83364cfb38d4336f438a8031
ykttdn <38461803+ykttdn@users.noreply.github.com>
2022-07-21 14:00:08 +0900

    Finish Lesson 11

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -42,6 +42,8 @@ app.component('product-display', {
         </button>
       </div>
     </div>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    <review-form @review-submitted="addReview"></review-form>
   </div>
   `,
   data() {
@@ -54,6 +56,7 @@ app.component('product-display', {
         { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
         { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
       ],
+      reviews: [],
     };
   },
   methods: {
@@ -65,6 +68,9 @@ app.component('product-display', {
     },
     updateVariant(index) {
       this.selectedVariant = index;
+    },
+    addReview(review) {
+      this.reviews.push(review);
     },
   },
   computed: {
